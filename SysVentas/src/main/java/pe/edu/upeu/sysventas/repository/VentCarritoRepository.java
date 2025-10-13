918d15bf3ac24cd6efbd971f8f990bfa797a3afa
abnerquispe-tech <quispeapazaabnerjosue@gmail.com>
2025-10-13 11:02:03 -0500

    Add files via upload

---
+++ SysVentas/src/main/java/pe/edu/upeu/sysventas/repository/VentCarritoRepository.java
@@ -0,0 +1,7 @@
+package pe.edu.upeu.sysventas.repository;
+
+
+import pe.edu.upeu.sysventas.model.VentCarrito;
+
+public interface VentCarritoRepository extends  ICrudGenericRepository<VentCarrito,Long>{
+}
